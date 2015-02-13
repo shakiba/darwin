c14457a7ba0f54b84f68627efcd006f90bc9cd37
Ali Shakiba <ali@shakiba.me>
2015-02-13 11:18:36 +0330

    select file/list list methods do map on files

--- darwin.js
+++ darwin.js
@@ -8,45 +8,41 @@
 var p = require('path');
 var fs = require('fs-extra');
 var glob = require("globby");
-var DNA = require("dna-js");
+var DNA = require("dna.js");
 
 var noisy = false;
 
 function Dir(base) {
-  noisy && console.log('Dir', base);
+  noisy && console.log('Dir:', base);
 
   this.path = function() {
     return base;
   };
 
   this.cd = function(dir) {
-    noisy && console.log('base', dir);
+    noisy && console.log('Dir.cd', base, dir);
     return new Dir(p.resolve(base, dir));
   };
 
-  this.file = function(name) {
-    return new File(base, name);
-  };
-
-  this.select = function(files) {
-    if (typeof files === 'string') {
-      files = [ files ];
+  this.select = function(select) {
+    noisy && console.log('Dir.select', base, select);
+    if (typeof select === 'string') {
+      return new File(base, select);
+    } else {
+      return new List(base, select.map(function(file) {
+        return new File(base, file);
+      }));
     }
-    return new Selection(base, files.map(function(file) {
-      return new File(base, file);
-    }));
   };
 
   this.glob = function(pattern, options) {
     noisy && console.log('glob', base, pattern, options);
     options || (options = {});
     options.cwd = base;
-
     files = glob.sync(pattern, options).map(function(file) {
       return new File(base, file);
     });
-
-    return new Selection(base, files);
+    return new List(base, files);
   };
 
   this.toString = function() {
@@ -54,38 +50,44 @@ function Dir(base) {
   };
 }
 
-function Selection(base, files) {
-  noisy && console.log('Selection', base, files.length);
+function List(base, files) {
+  noisy && console.log('List:', base, files.length);
 
   this.each = function(fn) {
     files.forEach(fn);
     return this;
   };
 
+  this.map = function(fn) {
+    return files.map(fn);
+  };
+
+  this.read = function(enoding) {
+    return this.map(function(file) {
+      return file.text(enoding);
+    });
+  };
+
   this.text = function(enoding) {
-    if (files.length) {
-      return files[0].text(enoding);
-    } else {
-      return '';
-    }
+    return this.map(function(file) {
+      return file.text(enoding);
+    });
   };
 
   this.json = function(enoding) {
-    return JSON.parse(this.text(enoding));
+    return this.map(function(file) {
+      return file.json(enoding);
+    });
   };
 
   this.dna = function(enoding) {
-    return DNA.parse(this.text(enoding));
-  };
-
-  this.join = function(joiner, enoding) {
-    return files.map(function(file) {
-      return file.text(enoding);
-    }).join(joiner);
+    return this.map(function(file) {
+      return DNA.parse(file.text(enoding));
+    });
   };
 
   this.copyTo = function(dest) {
-    return new Selection(dest.path(), files.map(function(file) {
+    return new List(dest.path(), files.map(function(file) {
       return file.copyTo(dest);
     }));
   };
@@ -96,18 +98,21 @@ function Selection(base, files) {
 }
 
 function File(base, file) {
+  noisy && console.log('File:', base, file);
+
   var path = p.resolve(base, file);
 
-  this.name = function() {
-    return file;
+  this.path = function(resolved) {
+    return resolved ? path : file;
   };
 
   this.read = function(enoding) {
+    noisy && console.log('File.read: ' + this);
     return fs.readFileSync(path, enoding);
   };
 
   this.write = function(content, enoding) {
-    noisy && console.log(this.toString());
+    noisy && console.log('File.write: ' + this);
     fs.outputFileSync(path, content, enoding);
   };
 
@@ -124,7 +129,7 @@ function File(base, file) {
   };
 
   this.copyTo = function(dest) {
-    var copy = dest.file(file);
+    var copy = dest.select(file);
     copy.write(this.read());
     return copy;
   };
